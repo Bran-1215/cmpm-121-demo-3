0d5b11fb0a78d353aa2a6102b59b7ca37cb8b7b0
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2024-11-11 14:27:35 -0800

    L16 Dialog requirements complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const button = document.createElement("button");
+button.textContent = "Button";
+
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+document.body.appendChild(button);
